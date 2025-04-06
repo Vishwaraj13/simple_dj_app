0d3a90fc0f71652dfdce7d4527c1376177776378
Vishwaraj <vishu.sankla@gmail.com>
2025-04-06 21:32:17 +0530

    wnmdw

--- wegoagain/settings.py
+++ wegoagain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c$dsa-1p!&-gu3!axkr2hv2-)cn8pbqihrv)r900te7p+krgw9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysimpledjango.azurewebsites.net']
+ALLOWED_HOSTS = ['mysimpledjango.azurewebsites.net','*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR/'static']
+STATICFILES_DIRS = [ BASE_DIR / 'static']
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
